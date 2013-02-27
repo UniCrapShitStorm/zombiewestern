177a69e94de3a0f5b8a0cd43c0e475d832d83624
simonsch <simon-s@online.de>
2013-02-27 17:03:11 +0100

    Added weapon-firerate

--- Project/Assets/Scripts/Weapon/Weapon.cs
+++ Project/Assets/Scripts/Weapon/Weapon.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 using System.Collections;
 
 public class Weapon : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+	private float decay = 0.001f;
+	private float decayTime = 0.0f;
+	public int maxAmmo;
+	private int currAmmo = 30;
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKey("mouse 0"))
+			Fire();
+	}
 	
+	void Fire() {
+		if(currAmmo<=0) return;
+		
+		if(decayTime>Time.time)
+			return;
+		
+		print("test");
+		
+		decayTime = Time.time+decay;
+		currAmmo--;
 	}
 }
