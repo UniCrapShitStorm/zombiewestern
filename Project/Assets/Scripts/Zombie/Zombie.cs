a01aff68439a1759856dfe2566b53c476e6f8466
H.B <henrik-bundt@web.de>
2013-02-28 16:55:31 +0100

    Most Basic Zombie Movement

--- Project/Assets/Scripts/Zombie/Zombie.cs
+++ Project/Assets/Scripts/Zombie/Zombie.cs
@@ -10,6 +10,25 @@ public class Zombie : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		GameObject zombie = GameObject.Find("human_animation_legs");
+		if(zombie==null)
+			return;
+		Vector3 z_pos = zombie.transform.position;
+		
+		GameObject player = GameObject.Find("First Person Controller");
+		if(player==null)
+			return;
+		Vector3 p_pos = player.transform.position;
+		Vector3 dir = -z_pos+p_pos;
+		if(norm2(dir)<=0.75f)
+			return;
+		CharacterMotor motor;
+        motor = GetComponent<CharacterMotor>();
+        motor.inputMoveDirection = transform.rotation * dir/norm2 (dir);
+	}
 	
+	private float norm2(Vector3 v) {
+		return Mathf.Sqrt(v.x*v.x+v.y*v.y+v.z*v.z);
 	}
 }
+	
