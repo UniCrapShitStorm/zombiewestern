61f0671720df7d6aa7f54361e4338123364267e7
philster91 <philster91@web.de>
2013-03-20 19:16:11 +0100

    waypoints

--- Project/Assets/Scripts/NavGraph.cs
+++ Project/Assets/Scripts/NavGraph.cs
@@ -4,16 +4,27 @@ using System.Collections.Generic;
 public class NavGraph : MonoBehaviour {
 	
 	public List<string> reachableWaypoints = new List<string>();
+	public GameObject[] waypoints;
+	
+	private RaycastHit hit;
+	private Vector3 rayDirection;
 	
 	// Use this for initialization
 	void Start () {
-		Vector3 direction = Vector3.forward;
-		RaycastHit hit;
-		if (Physics.Raycast(transform.position, Vector3.forward, out hit)) {
-			if (hit.transform.tag == "waypoint") {
-				reachableWaypoints.Add(hit.collider.gameObject.name);
+		waypoints = GameObject.FindGameObjectsWithTag("waypoint") as GameObject[];
+
+		foreach (GameObject wp_outer in waypoints) {
+			foreach (GameObject wp_inner in waypoints) {
+				rayDirection = wp_outer.transform.position - wp_inner.transform.position;
+				if (Physics.Linecast (wp_inner.transform.position, wp_outer.transform.position, out hit)) {
+					if (hit.transform.tag == "waypoint") {
+						string wp_inner_name = wp_inner.transform.name.ToString().Substring(2);
+						string wp_outer_name = wp_outer.transform.name.ToString().Substring(2);
+						reachableWaypoints.Add(wp_outer_name + " -> " + wp_inner_name);
+					}
+				}
 			}
-		}			
+		}
 	}
 	
 	// Update is called once per frame
