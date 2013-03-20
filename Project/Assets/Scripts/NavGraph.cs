289cc61046b7aaed37f63d888f0e792be84b5225
philster91 <philster91@web.de>
2013-03-20 17:44:19 +0100

    changed waypoints

--- Project/Assets/Scripts/NavGraph.cs
+++ Project/Assets/Scripts/NavGraph.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class NavGraph : MonoBehaviour {
-
+	
+	public List<string> reachableWaypoints = new List<string>();
+	
 	// Use this for initialization
 	void Start () {
-		
+		Vector3 direction = Vector3.forward;
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.forward, out hit)) {
+			if (hit.transform.tag == "waypoint") {
+				reachableWaypoints.Add(hit.collider.gameObject.name);
+			}
+		}			
 	}
 	
 	// Update is called once per frame
