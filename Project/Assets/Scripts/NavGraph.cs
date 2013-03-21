17684c72f142da9e91ff7cb2b368bae20347345b
philster91 <philster91@web.de>
2013-03-21 17:55:32 +0100

    added distance calculation between waypoints

--- Project/Assets/Scripts/NavGraph.cs
+++ Project/Assets/Scripts/NavGraph.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public class NavGraph : MonoBehaviour {
 	
 	public List<GameObject> reachableWaypoints = new List<GameObject>();
+	public List<float> distances = new List<float>();
 	public GameObject[] waypoints;
 	
 	// Use this for initialization
@@ -11,13 +12,23 @@ public class NavGraph : MonoBehaviour {
 		waypoints = GameObject.FindGameObjectsWithTag("waypoint") as GameObject[];
 
 		foreach (GameObject wp in waypoints) {
+			// check if waypoint can see another
 			if (!Physics.Linecast (transform.position, wp.transform.position)) {
-				reachableWaypoints.Add(wp);
+				// check if it is the waypoint itself
+				if (wp.transform.name != this.transform.name) {
+					reachableWaypoints.Add(wp);
+					distances.Add(calcDistance(transform.position, wp.transform.position));
+				}
 			}
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	}
+	
+	private float calcDistance (Vector3 a, Vector3 b) {
+		Vector3 c = a - b;
+		return Mathf.Sqrt(Mathf.Pow(c.x, 2)+Mathf.Pow(c.y, 2)+Mathf.Pow(c.z, 2));
 	}
 }
